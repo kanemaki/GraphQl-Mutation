91470f0c075568429fa01d11d24bbf52f19fe876
Alexandre Florentino <kanemaki@gmail.com>
2021-08-04 19:18:48 +0100

    feat: add function in db.js

--- data/db.js
+++ data/db.js
@@ -1,24 +1,29 @@
+let id = 1
+function nextId(){
+    return id++
+}
+
 const perfis = [
     { id: 1, nome: 'comum' },
     { id: 2, nome: 'administrador' }
 ]
 
 const usuarios = [{
-    id: 1,
+    id: nextId(),
     nome: 'João Silva',
     email: 'jsilva@zemail.com',
     idade: 29,
     perfil_id: 1,
     status: 'ATIVO'
 }, {
-    id: 2,
+    id: nextId(),
     nome: 'Rafael Junior',
     email: 'rafajun@wemail.com',
     idade: 31,
     perfil_id: 2,
     status: 'INATIVO'
 }, {
-    id: 3,
+    id: nextId(),
     nome: 'Daniela Smith',
     email: 'danismi@umail.com',
     idade: 24,
